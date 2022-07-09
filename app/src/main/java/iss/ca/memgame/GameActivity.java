68b8818b4c1002d2e3dcb640358c70f744f410d3
Chao <51955201+Chaochile@users.noreply.github.com>
2022-07-09 09:07:38 +0800

    Added correct mp3 when selection of two is correct

---
+++ app/src/main/java/iss/ca/memgame/GameActivity.java
@@ -0,0 +1,20 @@
+package iss.ca.memgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+    //Match image
+    //if selected1==selected2
+    //MediaPlayer correctSound = MediaPlayer.create(Game.this, R.raw.correct);
+    //				correctSound.start();
+
+}
