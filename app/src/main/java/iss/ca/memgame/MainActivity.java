fec1bde996731600bbffddd4009c22d482ca3bda
YI Ling <e0941863@u.nus.edu>
2022-07-09 08:02:13 +0800

    push 1 select 6 images and set to imageView at next activity

--- app/src/main/java/iss/ca/memgame/MainActivity.java
+++ app/src/main/java/iss/ca/memgame/MainActivity.java
@@ -1,11 +1,13 @@
 package iss.ca.memgame;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +22,7 @@ import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,6 +30,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Thread bkgdThread;
     InputStream in = null;
+    ArrayList<String> sixImages = new ArrayList<String>();
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,20 +77,19 @@ public class MainActivity extends AppCompatActivity {
                 bkgdThread = new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        try
-                        {
+                        try {
                             //using Jsoup to crawl the given url for a list of 20 image links
                             Document doc = Jsoup.connect(inputURL).get();
                             Elements elements = doc.getElementsByTag("img");
-                            List <String> urls = elements.stream().map(x->x.absUrl("src"))
-                                    .filter(x->!x.endsWith(".svg"))
+                            List<String> urls = elements.stream().map(x -> x.absUrl("src"))
+                                    .filter(x -> !x.endsWith(".svg"))
                                     .limit(20).collect(Collectors.toList());
 
                             //loop through list of image URLs and setting bitmap images to ImageView in layout grid
-                            for (int i=1; i<=20; i++) {
+                            for (int i = 1; i <= 20; i++) {
                                 if (Thread.interrupted())
                                     return;
-                                URL url = new URL(urls.get(i-1));
+                                URL url = new URL(urls.get(i - 1));
                                 URLConnection urlConn = url.openConnection();
                                 HttpURLConnection httpConn = (HttpURLConnection) urlConn;
                                 httpConn.connect();
@@ -105,11 +110,43 @@ public class MainActivity extends AppCompatActivity {
                                         img.setImageBitmap(bmpimg);
                                         progressBar.incrementProgressBy(1);
                                         progressText.setText("Downloading " + progressBar.getProgress() + " of 20 images");
+
+                                        if (img != null) {
+                                            img.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View view) {
+
+                                                    System.out.println("click success");
+                                                    System.out.println(sixImages.size());
+                                                    sixImages.add(url.toString());
+
+                                                }
+                                            });
+                                        }
+
+                                        btnSubmit = findViewById(R.id.btnSubmit);
+
+                                        btnSubmit.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                if (sixImages.size() == 6) {
+                                                    System.out.println("start intent");
+                                                    Intent intent = new Intent(MainActivity.this, MemoryGame.class);
+                                                    intent.putExtra("imgList", sixImages);
+                                                    startActivity(intent);
+
+                                                    System.out.println("intent okay");
+                                                }
+                                            }
+                                        });
                                     }
                                 });
                                 Thread.sleep(500);
                             }
                             progressText.setText("Download complete");
+
+
+
                         }
                         catch (
                                 MalformedURLException e)
@@ -128,5 +165,13 @@ public class MainActivity extends AppCompatActivity {
                 });bkgdThread.start();
             }
         });
+    }
+
+    public static String convertBitmapToString (Bitmap bitmap){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        String result = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        return result;
     }
 }
