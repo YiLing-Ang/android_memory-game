b4fce276015f6019872ad6ab7224b566e2e415fa
smk <moko944@gmail.com>
2022-07-09 15:29:18 +0800

    merged

--- app/src/main/java/iss/ca/memgame/GameActivity.java
+++ app/src/main/java/iss/ca/memgame/GameActivity.java
@@ -37,65 +37,6 @@ public class GameActivity extends AppCompatActivity {
     int seconds = 0;
     boolean running;
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_game);
-//
-//        running=true;
-//        runTimer();
-////
-////        btn = findViewById(R.id.finishBtn);
-////
-////        btn.setOnClickListener(new View.OnClickListener() {
-////
-////            @Override
-////            public void onClick(View view) {
-////
-////                running = false;
-////                seconds = 0;
-////                finish();
-////            }
-////        });
-//
-//        System.out.println("start memory game");
-//        Intent intent = getIntent();
-//        Resources r = getResources();
-//        String name = getPackageName();
-//        List<String> imgList = (List<String>) getIntent().getSerializableExtra("imgList");
-//
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                try {
-//                    //loop through list of image URLs and setting bitmap images to ImageView in layout grid
-//                    for (int i = 1; i <= 6; i++) {
-//
-//                        //converted to bitmap
-//                        Bitmap bmpimg = ImageDownload.downloadImg(imgList, i);
-//
-//                        //taking advantage of simple naming convention of ImageView to allow for looping
-//                        ImageView img = findViewById(r.getIdentifier("img" + i, "id", name));
-//                        ImageView img1 = findViewById(r.getIdentifier("img" + (i+6), "id", name));
-//
-//                        //UI thread started to allow setting of images
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                                img.setImageBitmap(bmpimg);
-//                                img1.setImageBitmap(bmpimg);
-//                            }
-//                        });
-//                    }
-//                }
-//                catch (Exception e)
-//                {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }).start();
-//    }
-
     TextView tv_p1, tv_p2,txtNumOfMatches;
 
     ImageView img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12;
@@ -121,10 +62,50 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         System.out.println("start memory game");
-        Intent intent = getIntent();
 
+        running=true;
+        runTimer();
+
+
+        System.out.println("start memory game");
+        Intent intent = getIntent();
         Resources r = getResources();
         String name = getPackageName();
+        List<String> imgList = (List<String>) getIntent().getSerializableExtra("imgList");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //loop through list of image URLs and setting bitmap images to ImageView in layout grid
+                    for (int i = 1; i <= 6; i++) {
+
+                        //converted to bitmap
+                        Bitmap bmpimg = ImageDownload.downloadImg(imgList, i);
+
+                        //taking advantage of simple naming convention of ImageView to allow for looping
+                        ImageView img = findViewById(r.getIdentifier("img" + i, "id", name));
+                        ImageView img1 = findViewById(r.getIdentifier("img" + (i+6), "id", name));
+
+                        //UI thread started to allow setting of images
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                img.setImageBitmap(bmpimg);
+                                img1.setImageBitmap(bmpimg);
+                            }
+                        });
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
         ArrayList<String> numbersList = (ArrayList<String>) getIntent().getSerializableExtra("imgList");
         if (bkgdThread != null) {
             bkgdThread.interrupt();
