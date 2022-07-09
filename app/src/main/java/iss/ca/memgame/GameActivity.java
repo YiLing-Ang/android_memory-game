8e61b84ebeed8d6d77c23ebf036fb73c48de5c8a
smk <moko944@gmail.com>
2022-07-09 11:02:42 +0800

    timer and return to main activity
    1.Timer
    2.return to main activity (by clicking the button currently)

--- app/src/main/java/iss/ca/memgame/GameActivity.java
+++ app/src/main/java/iss/ca/memgame/GameActivity.java
@@ -7,7 +7,11 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,8 +20,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Timer;
 
 public class GameActivity extends AppCompatActivity {
+
+    TextView timerText;
+    Button btn;
+    Timer timer;
+    int seconds = 0;
+    boolean running;
 
     InputStream in = null;
     private Thread bkgdThread;
@@ -25,6 +37,23 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        running=true;
+        runTimer();
+
+        btn = findViewById(R.id.finishBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                running = false;
+                seconds = 0;
+                finish();
+            }
+        });
+
         System.out.println("start memory game");
         Intent intent = getIntent();
 
@@ -96,6 +125,25 @@ public class GameActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void runTimer() {
+        timerText = findViewById(R.id.txtTime);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+                String time = String.format(Locale.getDefault(),"%d:%02d:%02d", hours, minutes, secs);
+                timerText.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
     //Match image
